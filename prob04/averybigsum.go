5e997136cba42f325b02ca5e8ec67ac80ef71220
Raul Freitas <raul.freitas@corp.globo.com>
2015-10-21 14:24:36 -0200

    Program 4

---
+++ prob04/averybigsum.go
@@ -0,0 +1,17 @@
+// nao fiz teste para esse pois se eu transformasse em um metodo, para nao usar scanf eu teria que armazenar
+//Tudo de uma vez em um vetor talvez. Isso ficaria igual ao metodo anterior...
+package main
+import "fmt"
+
+func main() {
+    var number int
+    var numbers, sum int64
+    numbers = 0
+    sum = 0
+    fmt.Scanf("%d", &number)
+    for i:=0; i<number; i++{
+        fmt.Scanf("%d", &numbers)
+        sum += numbers
+    }
+    fmt.Print(sum)
+}
