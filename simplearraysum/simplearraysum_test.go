b79cc00002a74e20c54743b79ce091594978a73b
Raul Freitas <raul.freitas@corp.globo.com>
2015-10-21 13:47:32 -0200

    Program 2 and 3

---
+++ simplearraysum/simplearraysum_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestSimpleArraySum(t *testing.T){
+  cases := []struct {
+    size, result int
+    array []int
+  }{
+    {0, 0, []int{}},
+    {1, 2, []int{2}},
+    {5, 15, []int{1, 2, 3, 4, 5}},
+  }
+  for _, c := range cases {
+    got := simpleArraySum(c.size, c.array)
+    if got != c.result {
+      t.Errorf("simpleArraySim(%d, %d) == %d, want %d", c.size, c.array, got, c.result)
+    }
+  }
+}
---
+++ solvemefirst2/solvemefirst_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestSolveMeFirst(t *testing.T) {
+	cases := []struct {
+		num1, num2, want uint32
+	}{
+		{1, 1, 2},
+		{1, 0, 1},
+		{0, 0, 0},
+	}
+	for _, c := range cases {
+		got := solveMeFirst(c.num1, c.num2)
+		if got != c.want {
+			t.Errorf("solveMeFirst(%d, %d) == %d, want %d", c.num1, c.num2, got, c.want)
+		}
+  }
+}
